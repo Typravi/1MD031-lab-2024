fe1319675bcb235a3eb488e33f86a1d7968e89f4
Typravi <typravi2@gmail.com>
2025-12-01 17:30:06 +0100

    Fixade med prettier så att det blev snyggt

--- src/views/DispatcherView.vue
+++ src/views/DispatcherView.vue
@@ -1,11 +1,12 @@
 <template>
-    <div id="orders">
-      <div id="orderList">
-        <div
-            v-for="(order, key) in orders"
-            :key="'order'+key"
-            v-on:mouseover="hoveredOrder = key"
-            v-on:mouseleave="hoveredOrder = null">
+  <div id="orders">
+    <div id="orderList">
+      <div
+        v-for="(order, key) in orders"
+        :key="'order' + key"
+        v-on:mouseover="hoveredOrder = key"
+        v-on:mouseleave="hoveredOrder = null"
+      >
         <div>#{{ key }}</div>
         <div id="customerDetails">
           <p>Name: {{ order.customerDetails.fullName }}</p>
@@ -13,114 +14,118 @@
           <p>Payment: {{ order.customerDetails.paymentMethod }}</p>
           <p>Gender: {{ order.customerDetails.gender }}</p>
         </div>
-          <ul>
-            <li v-for="(amount, name) in order.orderItems" :key="name">
-              {{ name }}: {{ amount }}
-            </li>
-          </ul>
-          <p id="statusText">Status: {{ order.status || 'Pending' }}</p>
-          <button id="InPreperation" v-on:click="changeStatus(key,'In preperation')">In preperation</button>
-          <button id="Done" v-on:click="changeStatus(key,'Done')">Done</button>
-        </div>
-        <button v-on:click="clearQueue">Clear Queue</button>
+        <ul>
+          <li v-for="(amount, name) in order.orderItems" :key="name">
+            {{ name }}: {{ amount }}
+          </li>
+        </ul>
+        <p id="statusText">Status: {{ order.status || "Pending" }}</p>
+        <button
+          id="InPreperation"
+          v-on:click="changeStatus(key, 'In preperation')"
+        >
+          In preperation
+        </button>
+        <button id="Done" v-on:click="changeStatus(key, 'Done')">Done</button>
       </div>
-      <div id="dots">
-          <div v-for="(order, key) in orders"
-              :key="'dots' + key"
-              :style="{ left: order.details.x + 'px', top: order.details.y + 'px' }"
-              :class="{ highlightedDot: hoveredOrder === key }">
-              {{ key }}
-          </div>
+      <button v-on:click="clearQueue">Clear Queue</button>
+    </div>
+    <div id="dots">
+      <div
+        v-for="(order, key) in orders"
+        :key="'dots' + key"
+        :style="{ left: order.details.x + 'px', top: order.details.y + 'px' }"
+        :class="{ highlightedDot: hoveredOrder === key }"
+      >
+        {{ key }}
       </div>
     </div>
-  </template>
-  <script>
-  import io from 'socket.io-client'
-  const socket = io("localhost:3000");
-  
-  export default {
-    name: 'DispatcherView',
-    data: function () {
-      return {
-        orders: null,
-        hoveredOrder: null
-      }
-    },
-    created: function () {
-      socket.on('currentQueue', data =>
-        this.orders = data.orders);
-    },
-    methods: {
-      clearQueue: function () {
-        socket.emit('clearQueue');
-      },
-      changeStatus: function(orderId, status) {
-        socket.emit('changeStatus', {orderId: orderId, status: status});
+  </div>
+</template>
+<script>
+import io from "socket.io-client";
+const socket = io("localhost:3000");
 
-      }
-    }
-  }
-  </script>
-  <style>
-  #orderList {
-    top:1em;
-    left:1em;
-    position: absolute;
-    z-index: 2;
-    color:black;
-    background: rgba(255,255,255, 0.5);
-    padding: 1em;
-  }
-  #dots {
-    position: relative;
-    margin: 0;
-    padding: 0;
-    background-repeat: no-repeat;
-    width:1920px;
-    height: 1078px;
-    cursor: crosshair;
-    background-image: url('/img/polacks.jpg');
-  }
-  
-  #dots div {
-    position: absolute;
-    background: black;
-    color: white;
-    border-radius: 10px;
-    width:20px;
-    height:20px;
-    text-align: center;
-  }
-  #customerDetails p {
-    font-size: 1rem;
-    padding: 0rem;
-    margin: 0rem;
-  }
-  #orderList ul {
-    font-size: 1rem;
-  }
-  #statusText {
-    font-size: 1rem;
-    margin: 0rem;
-    font-weight: bold;
-    color: green;
-  }
-  #InPreperation {
-    border-radius: 1rem;
-    margin-top: 0rem;
-    margin-bottom: 0.5rem;
-    display: flex;
-  }
-
-  #Done {
-    border-radius: 1rem;
-    margin-top: 0rem;
-    display: flex;
-  }
-  .highlightedDot {
-    transform: scale(2);
-    transition: 0.2s ease;
-    background-color: green !important;
+export default {
+  name: "DispatcherView",
+  data: function () {
+    return {
+      orders: null,
+      hoveredOrder: null,
+    };
+  },
+  created: function () {
+    socket.on("currentQueue", (data) => (this.orders = data.orders));
+  },
+  methods: {
+    clearQueue: function () {
+      socket.emit("clearQueue");
+    },
+    changeStatus: function (orderId, status) {
+      socket.emit("changeStatus", { orderId: orderId, status: status });
+    },
+  },
+};
+</script>
+<style>
+#orderList {
+  top: 1em;
+  left: 1em;
+  position: absolute;
+  z-index: 2;
+  color: black;
+  background: rgba(255, 255, 255, 0.5);
+  padding: 1em;
 }
-  </style>
-  
+#dots {
+  position: relative;
+  margin: 0;
+  padding: 0;
+  background-repeat: no-repeat;
+  width: 1920px;
+  height: 1078px;
+  cursor: crosshair;
+  background-image: url("/img/polacks.jpg");
+}
+
+#dots div {
+  position: absolute;
+  background: black;
+  color: white;
+  border-radius: 10px;
+  width: 20px;
+  height: 20px;
+  text-align: center;
+}
+#customerDetails p {
+  font-size: 1rem;
+  padding: 0rem;
+  margin: 0rem;
+}
+#orderList ul {
+  font-size: 1rem;
+}
+#statusText {
+  font-size: 1rem;
+  margin: 0rem;
+  font-weight: bold;
+  color: green;
+}
+#InPreperation {
+  border-radius: 1rem;
+  margin-top: 0rem;
+  margin-bottom: 0.5rem;
+  display: flex;
+}
+
+#Done {
+  border-radius: 1rem;
+  margin-top: 0rem;
+  display: flex;
+}
+.highlightedDot {
+  transform: scale(2);
+  transition: 0.2s ease;
+  background-color: green !important;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,16 @@
   <div class="homeViewBody">
     <header>
       <h1>Welcome to BurgerOnline</h1>
-      <img src="/img/burgers_resturang.png" alt="burger resturant" id="headerimage"/>
+      <img
+        src="/img/burgers_resturang.png"
+        alt="burger resturant"
+        id="headerimage"
+      />
     </header>
     <main>
       <section id="burgers">
         <h2>Select burger🍔</h2>
-        <p>This is where you choose your delicious burger  (づ•ᴗ•)づᯓ🍔</p>
+        <p>This is where you choose your delicious burger (づ•ᴗ•)づᯓ🍔</p>
 
         <Burger
           v-for="burger in burgers"
@@ -18,328 +22,343 @@
       </section>
       <section id="contact">
         <h2>Customer information</h2>
-        <p>This is where you provide necessary information so that we can deliver our great burgers to your doorstep 🍔 😋</p>
+        <p>
+          This is where you provide necessary information so that we can deliver
+          our great burgers to your doorstep 🍔 😋
+        </p>
         <h3>Delivery information:</h3>
-          <p>
-            <label for="fullName">Full name</label><br />
+        <p>
+          <label for="fullName">Full name</label><br />
+          <input
+            type="text"
+            id="fullName"
+            v-model="fullName"
+            required="required"
+            placeholder="Full name"
+          />
+        </p>
+        <p>
+          <label for="email">E-mail</label><br />
+          <input
+            type="email"
+            id="email"
+            v-model="email"
+            required="required"
+            placeholder="E-mail address"
+          />
+        </p>
+
+        <h4>Payment method</h4>
+        <select id="payment_method" v-model="paymentMethod">
+          <option value="Swish">Swish</option>
+          <option value="Klarna">Klarna</option>
+          <option value="Card">Card</option>
+          <option value="Bitcoin">Bitcoin</option>
+          <option value="Cash">Cash</option>
+        </select>
+
+        <h4>Gender:</h4>
+        <div class="gender-group">
+          <label>
+            <input type="radio" value="Man" v-model="gender" />
+            Man
+          </label>
+
+          <label>
+            <input type="radio" value="Woman" v-model="gender" />
+            Woman
+          </label>
+
+          <label>
             <input
-              type="text"
-              id="fullName"
-              v-model="fullName"
-              required="required"
-              placeholder="Full name"
+              type="radio"
+              value="Do not wish to provide"
+              v-model="gender"
             />
-            
-          </p>
-          <p>
-             <label for="email">E-mail</label><br />
-              <input
-                type="email"
-                id="email"
-                v-model="email"
-                required="required"
-                placeholder="E-mail address"
-              />
-              
-          </p>
-      
-          <h4>Payment method</h4>
-            <select id="payment_method" v-model="paymentMethod">
-              <option value="Swish">Swish</option>
-              <option value="Klarna">Klarna</option>
-              <option value="Card">Card</option>
-              <option value="Bitcoin">Bitcoin</option>
-              <option value="Cash">Cash</option>
-            </select>
-            
-
-          <h4>Gender:</h4>
-            <div class="gender-group">
-              <label>
-                <input type="radio" value="Man" v-model="gender" />
-                  Man
-              </label>
-
-              <label>
-                <input type="radio" value="Woman" v-model="gender" />
-                Woman
-              </label>
-
-              <label>
-                <input type="radio" value="Do not wish to provide" v-model="gender" />
-                Do not wish to provide
-              </label>
-            </div>
-            
-      
-          </section>
-        <div id="mapContainer">
-          <div id="map" v-on:click="setLocation">
-            Press to set delivery location
-            <div
-              id="customerDot"
-              v-bind:style="{ left: location.x + 'px', top: location.y + 'px' }"
-            >T</div>
+            Do not wish to provide
+          </label>
+        </div>
+      </section>
+      <div id="mapContainer">
+        <div id="map" v-on:click="setLocation">
+          Press to set delivery location
+          <div
+            id="customerDot"
+            v-bind:style="{ left: location.x + 'px', top: location.y + 'px' }"
+          >
+            T
           </div>
         </div>
-      
+      </div>
+
       <div>
-            <button id="sendButton" type="button" v-on:click="placeOrder"> Send Delivery
-              <img src="/img/sendButtondelivery.jpg" alt="delivery icon" />
-          </button>
+        <button id="sendButton" type="button" v-on:click="placeOrder">
+          Send Delivery
+          <img src="/img/sendButtondelivery.jpg" alt="delivery icon" />
+        </button>
       </div>
       <div id="receipt">
         <div v-if="orderStatus" id="customerStatus">
           <p>Order status: {{ orderStatus }}</p>
-      </div> 
+        </div>
       </div>
     </main>
-    </div>
-    <hr />
-    <footer>
-      <p>Copyright &copy; 2025 BurgerOnline</p>
-    </footer>
-  
+  </div>
+  <hr />
+  <footer>
+    <p>Copyright &copy; 2025 BurgerOnline</p>
+  </footer>
 </template>
 
 <script>
-import Burger from '../components/OneBurger.vue'
-import io from 'socket.io-client'
-import Menu from '../assets/menu.json'
+import Burger from "../components/OneBurger.vue";
+import io from "socket.io-client";
+import Menu from "../assets/menu.json";
 
 function MenuItem(name, url, taste, gluten, lactose) {
-  this.name = name
-  this.url = url
-  this.taste = taste
-  this.gluten = gluten
-  this.lactose = lactose
+  this.name = name;
+  this.url = url;
+  this.taste = taste;
+  this.gluten = gluten;
+  this.lactose = lactose;
 }
 
 const socket = io("localhost:3000");
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    Burger
+    Burger,
   },
   data: function () {
     return {
       burgers: Menu,
-      fullName: '',
-      email: '',
-      paymentMethod: 'Swish', // default value
-      gender: 'Man', // default value
+      fullName: "",
+      email: "",
+      paymentMethod: "Swish", // default value
+      gender: "Man", // default value
       orderedBurgers: {},
       location: {
-        x:0,
-        y:0
+        x: 0,
+        y: 0,
       },
       currentOrderId: null,
-      orderStatus: null
-    }
+      orderStatus: null,
+    };
   },
-  created () {
-    socket.on('currentQueue', (data) => {
+  created() {
+    socket.on("currentQueue", (data) => {
       if (!this.currentOrderId) return;
       const myOrder = data.orders[this.currentOrderId];
       if (myOrder) {
-        this.orderStatus = myOrder.status || 'pending';
+        this.orderStatus = myOrder.status || "pending";
       }
     });
   },
   methods: {
     getOrderNumber: function () {
-      return Math.floor(Math.random()*100000);
+      return Math.floor(Math.random() * 100000);
     },
-    setLocation: function(event) {
-      var offset = {x: event.currentTarget.getBoundingClientRect().left,
-                    y: event.currentTarget.getBoundingClientRect().top};
-      
-                    this.location = { x: event.clientX - 30 - offset.x,
-                                      y: event.clientY - 30 - offset.y };
+    setLocation: function (event) {
+      var offset = {
+        x: event.currentTarget.getBoundingClientRect().left,
+        y: event.currentTarget.getBoundingClientRect().top,
+      };
+
+      this.location = {
+        x: event.clientX - 30 - offset.x,
+        y: event.clientY - 30 - offset.y,
+      };
     },
 
-
-    updateOrder ({ burger, amount }) {
-    if (amount > 0) {
-      this.orderedBurgers[burger.name] = amount
-    } else {
-      delete this.orderedBurgers[burger.name]
-    }
+    updateOrder({ burger, amount }) {
+      if (amount > 0) {
+        this.orderedBurgers[burger.name] = amount;
+      } else {
+        delete this.orderedBurgers[burger.name];
+      }
     },
-    placeOrder () {
-      if (!this.fullName || !this.email || Object.keys(this.orderedBurgers).length === 0) {
-            alert("Please fill in all fields and select at least one burger.");
-            return;}
+    placeOrder() {
+      if (
+        !this.fullName ||
+        !this.email ||
+        Object.keys(this.orderedBurgers).length === 0
+      ) {
+        alert("Please fill in all fields and select at least one burger.");
+        return;
+      }
       if (this.location.x === 0 && this.location.y === 0) {
-            alert("Please choose a delivery location on the map.");
-            return;}
-      
-      if (this.fullName || this.email || !Object.keys(this.orderedBurgers).length === 0|| !this.location.x === 0 && !this.location.y === 0) {
-            const orderNumber = this.getOrderNumber();
-            this.currentOrderId = orderNumber;
+        alert("Please choose a delivery location on the map.");
+        return;
+      }
 
-            let receipt = "Order Receipt\n";
-            receipt += 'Order number: ' + orderNumber + '\n';
-            receipt += `Name: ${this.fullName}\n`;
-            receipt += `Email: ${this.email}\n`;
-            receipt += `Payment Method: ${this.paymentMethod}\n`;
-            receipt += `Gender: ${this.gender}\n`;
+      if (
+        this.fullName ||
+        this.email ||
+        !Object.keys(this.orderedBurgers).length === 0 ||
+        (!this.location.x === 0 && !this.location.y === 0)
+      ) {
+        const orderNumber = this.getOrderNumber();
+        this.currentOrderId = orderNumber;
 
-            socket.emit("addOrder", {
-                                  orderId: orderNumber,
-                                  details: this.location,
-                                  orderItems: this.orderedBurgers,
-                                  customerDetails: {
-                                    fullName: this.fullName,
-                                    email: this.email,
-                                    paymentMethod: this.paymentMethod,
-                                    gender: this.gender
-                                  }
-                                });
-        alert(receipt)
-            }
-    }
-  }
-}
+        let receipt = "Order Receipt\n";
+        receipt += "Order number: " + orderNumber + "\n";
+        receipt += `Name: ${this.fullName}\n`;
+        receipt += `Email: ${this.email}\n`;
+        receipt += `Payment Method: ${this.paymentMethod}\n`;
+        receipt += `Gender: ${this.gender}\n`;
+
+        socket.emit("addOrder", {
+          orderId: orderNumber,
+          details: this.location,
+          orderItems: this.orderedBurgers,
+          customerDetails: {
+            fullName: this.fullName,
+            email: this.email,
+            paymentMethod: this.paymentMethod,
+            gender: this.gender,
+          },
+        });
+        alert(receipt);
+      }
+    },
+  },
+};
 </script>
 
 <style>
-  @import url('https://fonts.googleapis.com/css2?family=Agbalumo&family=Cormorant:wght@700&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Agbalumo&family=Cormorant:wght@700&display=swap");
 body {
-    font-size: 2rem;
-    font-family:'Times New Roman', Times, serif;
-    background-color: #340a05;
+  font-size: 2rem;
+  font-family: "Times New Roman", Times, serif;
+  background-color: #340a05;
 }
 p {
-    font-size: 1em;
-    padding: 0.5rem 1rem;
-    line-height: 1.5;
+  font-size: 1em;
+  padding: 0.5rem 1rem;
+  line-height: 1.5;
 }
 
 h1 {
-    font-family: 'Agbalumo';
-    font-size: 4rem;
-    color: whitesmoke;
+  font-family: "Agbalumo";
+  font-size: 4rem;
+  color: whitesmoke;
 }
 
 main {
-    background-color: white;
-    background: url("/img/burgerPattern.png");
-    background-size: cover;
-    padding-top: 1rem;
-    border-radius: 2rem;
+  background-color: white;
+  background: url("/img/burgerPattern.png");
+  background-size: cover;
+  padding-top: 1rem;
+  border-radius: 2rem;
 }
 
 header {
-    position: relative;
-    height: 50%;
-    overflow: hidden;
-    margin: 0em 2em 0em 2em;
+  position: relative;
+  height: 50%;
+  overflow: hidden;
+  margin: 0em 2em 0em 2em;
 }
 
 header img {
-    width: 100%;
-    height: 20rem;
-    object-fit: cover;
-    opacity: 0.5;
-    display: block;
-    border-radius: 2rem;
+  width: 100%;
+  height: 20rem;
+  object-fit: cover;
+  opacity: 0.5;
+  display: block;
+  border-radius: 2rem;
 }
 
 header h1 {
-    position: absolute; /*positionerar texten i bilden */
-    top: 50%;
-    width:100%;
-    margin: 0 auto;
-    text-align: center;
+  position: absolute; /*positionerar texten i bilden */
+  top: 50%;
+  width: 100%;
+  margin: 0 auto;
+  text-align: center;
 }
 
-
 .ingredients {
-    font-weight: bold;
+  font-weight: bold;
 }
 
 #contact {
-    border-color: black;
-    color: white;
+  border-color: black;
+  color: white;
 }
 #contact h2 {
-    font-size: 1.3em;
-    margin: 0.5em;
-    font-weight: bold;
+  font-size: 1.3em;
+  margin: 0.5em;
+  font-weight: bold;
 }
 #contact h3 {
-    font-size: 1em;
-    margin: 0.5em;
+  font-size: 1em;
+  margin: 0.5em;
 }
 #contact h4 {
-    font-size: 0.8em;
-    margin-left: 0.5em;
-    margin-top: 0.5em;
+  font-size: 0.8em;
+  margin-left: 0.5em;
+  margin-top: 0.5em;
 }
 #contact p {
-    margin: 0.5em;
-    font-size: 0.5em;
+  margin: 0.5em;
+  font-size: 0.5em;
 }
 #contact .gender-group label {
-    display: block;               /* one per line */
-    font-size: 1rem;
-    margin: 0.2rem 0;
+  display: block; /* one per line */
+  font-size: 1rem;
+  margin: 0.2rem 0;
 }
 #contact .gender-group label:hover {
-    color: green;
-    cursor: pointer;
+  color: green;
+  cursor: pointer;
 }
 #payment_method {
-    margin: 1rem;
+  margin: 1rem;
 }
 
-
 button:hover {
-    color: greenyellow;
-    cursor: pointer; /*ändrar mus*/
+  color: greenyellow;
+  cursor: pointer; /*ändrar mus*/
 }
 
 section {
-    margin: 0.5em 2em;
-    padding: 2rem;
-    border: 2px dashed;
+  margin: 0.5em 2em;
+  padding: 2rem;
+  border: 2px dashed;
 }
 
 button {
-    margin: 3rem;
-    padding: 1rem;
-    background-color: white;
+  margin: 3rem;
+  padding: 1rem;
+  background-color: white;
 }
 #sendButton {
-    font-size: 1rem;
-    font-family: Helvetica, sans-serif;
-    font-weight: bold;
-    border-radius: 1.5rem;
+  font-size: 1rem;
+  font-family: Helvetica, sans-serif;
+  font-weight: bold;
+  border-radius: 1.5rem;
 }
 #sendButton img {
-    width: 2rem;
-    height: auto;
-    vertical-align: middle;
-    margin-left: 0.5rem;
+  width: 2rem;
+  height: auto;
+  vertical-align: middle;
+  margin-left: 0.5rem;
 }
 #sendButton:hover {
-    background-image: none;
-    background-color: #1a9b6a;
-    color: white;
-  }
+  background-image: none;
+  background-color: #1a9b6a;
+  color: white;
+}
 #sendButton:hover img {
   content: url("/img/sendButtonHover.png");
 }
 
-section div{
-    margin: 1rem;
-    
+section div {
+  margin: 1rem;
 }
 
 div {
-    margin: 1rem;
+  margin: 1rem;
 }
 
 #burgers {
@@ -351,7 +370,7 @@ div {
   gap: 1rem;
 }
 
-#burgers > h2, 
+#burgers > h2,
 #burgers > p {
   grid-column: 1 / -1; /* rubrik och text över hela bredden */
   margin-bottom: 0.2rem;
@@ -380,7 +399,7 @@ div {
 #customerDot {
   position: absolute;
   background: #340a05;
-  width: 30px;  /* denna och under är samma som i addorder */
+  width: 30px; /* denna och under är samma som i addorder */
   height: 30px;
   color: whitesmoke;
   border-radius: 15px;
@@ -393,5 +412,4 @@ footer {
 #customerStatus {
   color: white;
 }
-
 </style>
