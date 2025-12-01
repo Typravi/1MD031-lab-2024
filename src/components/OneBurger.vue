fdeccd938f1e041108267c44d0db28a90ac4fda7
Typravi <typravi2@gmail.com>
2025-12-01 17:29:08 +0100

    Fixade med prettier

--- src/components/OneBurger.vue
+++ src/components/OneBurger.vue
@@ -4,80 +4,77 @@
     <img :src="burger.url" :alt="burger.name" />
 
     <ul>
-      <li><span class="ingredients">{{ burger.taste }} taste</span></li>
+      <li>
+        <span class="ingredients">{{ burger.taste }} taste</span>
+      </li>
       <li v-if="burger.gluten"><span class="ingredients">Gluten</span></li>
       <li v-if="burger.lactose"><span class="ingredients">Lactose</span></li>
     </ul>
     <button id="decreaseButton" type="button" v-on:click="decrease">-</button>
-      {{ amountOrdered }}
-      <button id="increaseButton" type="button" v-on:click="increase">+</button>
-    
-
+    {{ amountOrdered }}
+    <button id="increaseButton" type="button" v-on:click="increase">+</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'OneBurger',
+  name: "OneBurger",
   props: {
-    burger: Object
+    burger: Object,
   },
   data: function () {
     return {
-      amountOrdered:0,
+      amountOrdered: 0,
     };
   },
   methods: {
-    decrease () {
+    decrease() {
       if (this.amountOrdered > 0) {
-        this.amountOrdered--
-        this.$emit('update-order', {
+        this.amountOrdered--;
+        this.$emit("update-order", {
           burger: this.burger,
-          amount: this.amountOrdered
-        })
+          amount: this.amountOrdered,
+        });
       }
     },
-    increase () {
-      this.amountOrdered++
-      this.$emit('update-order', {
+    increase() {
+      this.amountOrdered++;
+      this.$emit("update-order", {
         burger: this.burger,
-        amount: this.amountOrdered
-      })
-    }
-  }
-}
-
-
+        amount: this.amountOrdered,
+      });
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.burger{
-    background-color: black;
-    color: white;
-    text-align: center;
-   
+.burger {
+  background-color: black;
+  color: white;
+  text-align: center;
 }
 .burger h2 {
-    font-size: 1.5rem;
+  font-size: 1.5rem;
 }
 
-.burger h2, p {
-    text-align: center - 0.5rem;
+.burger h2,
+p {
+  text-align: center - 0.5rem;
 }
 
 .burger img {
-    height: 200px;
-    width: 200px;
-    display: block;
-    margin-left: 2.5rem;
+  height: 200px;
+  width: 200px;
+  display: block;
+  margin-left: 2.5rem;
 }
 
 .burger ul {
-    list-style-type: disc;
-    list-style-position: inside;
-    text-align: left;
-
+  list-style-type: disc;
+  list-style-position: inside;
+  text-align: left;
 }
 .burger button {
   border-radius: 50%;
@@ -86,13 +83,13 @@ export default {
   font-weight: bolder;
 }
 #increaseButton:hover {
-    background-color: green;
-    cursor: pointer;
-    color: white;
+  background-color: green;
+  cursor: pointer;
+  color: white;
 }
 #decreaseButton:hover {
-    background-color: red;
-    cursor: pointer;
-    color: white;
+  background-color: red;
+  cursor: pointer;
+  color: white;
 }
 </style>
